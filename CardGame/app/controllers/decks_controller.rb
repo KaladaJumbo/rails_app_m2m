ab6cafb275f8de712ba764a21a6b5b8aa05bc1ff
KaladaJumbo <wkalada1@gmail.com>
2020-09-22 14:08:32 -0400

    finished all the creates and new controllers

--- CardGame/app/controllers/decks_controller.rb
+++ CardGame/app/controllers/decks_controller.rb
@@ -17,8 +17,18 @@ class DecksController < ApplicationController
     end
 
     def create 
+        player = Player.find_or_create_by(name: params[:deck][:player])
+        deck = Deck.find_or_create_by(adam_hates_strong_params(:name, :style, :decktype))
+        deck.player = player
+        deck.save
+        redirect_to deck_path(deck)
 
     end 
+
+    def addcard
+        @cards = Card.all
+        render "addcard"
+    end
 
     def edit
 
@@ -29,6 +39,14 @@ class DecksController < ApplicationController
     end
 
     def destroy
+
+    end
+
+    private 
+
+    def adam_hates_strong_params(*args)
+
+        params.require(:deck).permit(*args)
 
     end
 
--- CardGame/config/routes.rb
+++ CardGame/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :cards
   resources :decks
   resources :players
+  get "decks/:id/addcard", to: "decks#addcard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
